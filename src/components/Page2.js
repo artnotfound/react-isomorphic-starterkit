425a490928556dd51dc639dbd04bd59e146ef1cc
manny404 <manny@art404.com>
2016-02-28 14:30:45 -0500

    broken react-router page reload

---
+++ src/components/Page2.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Page2 extends React.Component {
+  render () {
+    return <h1>{'HERES PAGE 2'}</h1>
+  }
+}
+
+export default Page2
+
--- src/containers/routes.js
+++ src/containers/routes.js
@@ -1,13 +1,17 @@
 import React from "react";
 import {Router, Route} from "react-router";
-
 import Main from "./Main";
+import Page from '../components/Page';
+import Page2 from '../components/Page2';
 
 /**
  * The React Router routes for both the server and the client.
  */
 module.exports = (
-	<Router>
-		<Route path="/" component={Main} />
-	</Router>
+  <Router>
+    <Route path="/" component={Main}>
+      <Route path="page" component={Page}/>
+      <Route path="page2" component={Page2}/>
+    </Route>
+  </Router>
 );
